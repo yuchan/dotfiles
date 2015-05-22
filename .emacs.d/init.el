8eda65e61e2d70d1e0e26eb3cfdb36033ba15e9e
Yusuke Ohashi <yusuke@junkpiano.me>
2015-05-22 17:19:58 +0900

    update .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,33 @@
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    init-loader
+    auto-complete
+    cider
+    clojure-mode
+    feature-mode
+    magit
+    markdown-mode
+    org
+    ox-reveal
+    projectile
+    projectile-rails
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+			   when (not (package-installed-p x))
+			   collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+      (package-install pkg))))
+
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
