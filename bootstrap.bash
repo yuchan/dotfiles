26102ebef967d1ba4db27ca689e9961a1f69dab3
Yusuke Ohashi <github@junkpiano.me>
2015-11-15 11:15:33 +0900

    remove atom init.

--- bootstrap.bash
+++ bootstrap.bash
@@ -7,10 +7,3 @@ for dotfile in $(find . -maxdepth 1 \( -iname '.*' -not -iname '.git*' -not -ina
     rm -rf ${HOME}/${dotfile} || continue
     ln -s ${PWD}/${dotfile} ${HOME}/${dotfile}
 done
-
-hash atom 2>/dev/null || { echo >&2 "atom is not installed."; }
-hash apm 2>/dev/null || { echo >&2 "apm is not installed."; }
-
-if hash apm 2>/dev/null; then
-  apm install --packages-file my-packages.txt
-fi
