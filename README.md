e0302cebe772612064d5b401a80263777d66a61f
Yusuke Ohashi <github@junkpiano.me>
2015-12-22 20:15:31 +0900

    modified setup script.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ dotfiles
 =============
 
 These are my dotfiles.
+
+Prerequisite
+--------------
+
+    $ ./bootstrap.bash
+
+and then, please set terminal font from names in fonts.
--- bootstrap.bash
+++ bootstrap.bash
@@ -7,3 +7,15 @@ for dotfile in $(find . -maxdepth 1 \( -iname '.*' -not -iname '.git*' -not -ina
     rm -rf ${HOME}/${dotfile} || continue
     ln -s ${PWD}/${dotfile} ${HOME}/${dotfile}
 done
+
+if which z >/dev/null; then
+    echo 'z is already installed.'
+else
+    brew install z
+fi
+
+currentdir=$PWD
+
+cd $currentdir/powerline-shell && cp config.py.dist config.py && ./install.py
+cd $currentdir/fonts && ./install.sh
+cd $currentdir
